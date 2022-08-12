4799c34670086091dc8c2eea5117d14307c8491b
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-08-12 15:24:22 +1000

    scaffold

---
+++ app.py
@@ -0,0 +1,14 @@
+def intersection(set1, set2):
+	# set1 and set2 are lists
+	# return the intersection
+    pass
+
+def union(set1, set2):
+	# set1 and set2 are lists
+	# return the union
+    pass
+
+def symmetric_difference(set1, set2):
+	# set1 and set2 are lists
+	# return the symmetric difference
+    pass
