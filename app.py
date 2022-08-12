3e4f1cf9c416d5ce4ef5189d076036415b30c8df
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-08-12 15:37:14 +1000

    +w union func

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ def intersection(set1, set2):
     pass
 
 def union(set1, set2):
-	# set1 and set2 are lists
-	# return the union
-    pass
+    ul = set1 + set2
+    result = set(ul)
+    return result
 
 def symmetric_difference(set1, set2):
 	# set1 and set2 are lists
 	# return the symmetric difference
     pass
+
+print(union([1,2,3], [3,4,5]))
