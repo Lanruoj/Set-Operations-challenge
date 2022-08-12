fcb8a32cfc92e8d858f3b3752e94a0617a3229c6
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-08-12 15:51:41 +1000

    +w union func easy, +w union func hard

--- app.py
+++ app.py
@@ -3,14 +3,22 @@ def intersection(set1, set2):
 	# return the intersection
     pass
 
+# EASY WAY...
+# def union(set1, set2):
+#     ul = set1 + set2
+#     result = set(ul)
+#     return result
+
 def union(set1, set2):
     ul = set1 + set2
-    result = set(ul)
-    return result
+    for i in ul:
+        if (i in set1) and (i in set2):
+            ul.remove(i)
+    return ul
 
 def symmetric_difference(set1, set2):
 	# set1 and set2 are lists
 	# return the symmetric difference
     pass
 
-print(union([1,2,3], [3,4,5]))
+print(union(["a", "b", "c"], ["d", "e", "f"]))
